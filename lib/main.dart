dd65856c8934a8e940c6d5e9707ff8a0933e7d15
Zeynep Menekşe <zeynep.menekse@sabanciuniv.edu>
2025-12-15 20:39:59 +0300

    chore: Add header comments to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+// BookMate - A Social Reading App
+// CS310 Mobile Application Development
+// Step 3: Firebase Integration
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
